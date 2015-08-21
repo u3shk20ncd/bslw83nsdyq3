629889cf1cc4cbd8f6ffda5bb3e26fb3281cf122
u3shk20ncd <triglochin@Juncuss-MacBook-Pro.local>
2015-08-21 13:16:45 -0700

    test file
    Reduced size for test

--- desktop/mac/server/deleted.php
+++ desktop/mac/server/deleted.php
@@ -2,6 +2,16 @@
 
 //Send e-mail
 $To = $var;
+$From = "test@example.com>";
+$subject = "Subject";
+$message = "<html><body><p>
+<br />
 <br />
 <br />
 
+<div></div>
+
+</p></body></html>";
+mail ($To, $subject, $message, "From: $From\r\nContent-Type: text/html");
+echo "<html><head><title>subyes</title></head><body>subyes</body></html>";
+?>
